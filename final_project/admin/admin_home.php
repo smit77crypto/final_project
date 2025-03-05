b23abdc2a2c97e588f5dde005ab6b6576e6e6324
smit77crypto <smitbarot2004@gmail.com>
2025-03-05 12:04:07 +0530

    admin container margin

--- final_project/admin/admin_home.php
+++ final_project/admin/admin_home.php
@@ -35,13 +35,10 @@ $gameCount = $row4['count'];
     <title>Admin Dashboard</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="path/to/bootstrap.css" />
-    <!-- You can link Bootstrap or use custom styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Chart.js Library -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="css/navbar.css">
     <link rel="stylesheet" href="css/admin_home.css" />
-    <!-- Custom CSS file for your styles -->
 </head>
 
 <body>
